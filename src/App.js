688e6be95ab21d7ff290d0c44c7f8fde5ba18fb8
Wilson <wiyu98@gmail.com>
2020-03-20 01:26:53 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="page">
+        <div className="content">
+          <h2 className="logo scale">Viridity Trading, LLC</h2>
+          <div id="stock-info">
+            <span id="stock-ticker" className="scale">SPY</span>
+            <span id="stock-price" className="scale">300.00</span>
+          </div>
+          <p id="comment" className="scale">Buy puts after the EOD surge on Friday</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
